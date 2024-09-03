e8d0476204fae80282406b599544d4ede0cb7606
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-09-03 14:59:09 +0300

    OOP

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+
+// function hello(){
+//     echo 'hello';
+// }
+// $var = 'hello';
+// hello();
+// $var();
+
+class Box {
+    public  $width;
+    private  $height;
+    protected  $length;
+
+    public function __construct($width, $height,$length){
+        $this->width = $width;
+        $this->height = $height;
+        $this->length = $length;
+    }
+    public function __toString(){
+        return 'box';
+    }
+
+    public function __get($name){
+        var_dump($name);
+    }
+
+    public function __set($name, $value){
+        var_dump($name, $value);
+    }
+
+    public function __call($name, $args){
+        var_dump($name, $args);
+    }
+    public function __invoke(...$args){
+        var_dump($args);
+    }
+    public function getHeight(){
+        return $this->height;
+    }
+    public function setHeight(int $height){
+        if($height < 0){
+            $height = 0;
+        }
+        $this->height = $height;
+    }
+
+
+}
+
+class MetalBox extends Box {
+    public function getHeight2(){
+        return $this->height;
+    }
+    public function getLength(){
+        return $this->length;
+    }
+    public function setLength(int $length){
+        if($length < 0){
+            $length = 0;
+        }
+        $this->length = $length;
+    }
+}
+
+
+// $num = 1;
+// $num2 = $num;
+// $num2 = 2;
+// var_dump($num2);
+// var_dump($num);
+
+
+$box = new MetalBox(1,2,3);
+//$box->length = 12;
+$box->setHeight(-20);
+$box->getLength();
+echo $box;
+$box(123,'asdas', true);
+$box->hello;
+$box->hello = 5;
+$box->world(1,'asdas', 4);
+// $box2 = clone $box;
+// $box2->width = 40;
+// var_dump($box2);
+ var_dump($box);
