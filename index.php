4cc305111b565a4be2bad3de61cda5ec7a082a74
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-09-03 15:21:04 +0300

    interface example

--- index.php
+++ index.php
@@ -1,102 +1,38 @@
 <?php
 
-// function hello(){
-//     echo 'hello';
-// }
-// $var = 'hello';
-// hello();
-// $var();
+class ConsoleLogger implements Logger {
+    public function log($message){
+        echo $message . "\n";
+    }
+}
 
-class Box {
-    public  $width;
-    private  $height;
-    protected  $length;
+class Task {
+    public function work(Logger $logger){
 
-    public function __construct($width, $height,$length){
-        $this->width = $width;
-        $this->height = $height;
-        $this->length = $length;
-    }
-    public function __toString(){
-        return 'box';
-    }
-
-    public function __get($name){
-        var_dump($name);
-    }
-
-    public function __set($name, $value){
-        var_dump($name, $value);
-    }
-
-    public function __call($name, $args){
-        var_dump($name, $args);
-    }
-    public function __invoke(...$args){
-        var_dump($args);
-    }
-    public function getHeight(){
-        return $this->height;
-    }
-    public function setHeight(int $height){
-        if($height < 0){
-            $height = 0;
+        for($i=0;$i<10;$i++){
+            $logger->log($i);
         }
-        $this->height = $height;
-    }
-
-
-}
-
-class MetalBox extends Box {
-    use Material;
-    public function getHeight2(){
-        return $this->height;
-    }
-    public function getLength(){
-        return $this->length;
-    }
-    public function setLength(int $length){
-        if($length < 0){
-            $length = 0;
-        }
-        $this->length = $length;
-    }
-}
-class Ball {
-    use Material;
-}
-
-trait Material {
-    private $material; 
-    public function setMaterial($material){
-        $this->material = $material;
-    }
-    public function getMaterial(){
-        return $this->material;
     }
 }
 
+interface Logger {
+    public function log($message);
+}
 
-// $num = 1;
-// $num2 = $num;
-// $num2 = 2;
-// var_dump($num2);
-// var_dump($num);
+//
+class NothingLogger implements Logger {
+    public function log($message){
+        
+    }
+}
 
-
-$box = new MetalBox(1,2,3);
-//$box->length = 12;
-$box->setHeight(-20);
-$box->getLength();
-echo $box;
-$box(123,'asdas', true);
-$box->hello;
-$box->hello = 5;
-$box->world(1,'asdas', 4);
-// $box2 = clone $box;
-// $box2->width = 40;
-// var_dump($box2);
- var_dump($box);
- $box2 = new Box(1,2,3);
- var_dump($box2);
+class FileLogger implements Logger {
+    public function log($message){
+        $file = fopen('./log.log', 'a');
+        fwrite($file, $message . "\n");
+        fclose($file);
+    }
+}
+$logger = new NothingLogger();
+$task = new Task();
+$task->work($logger);
